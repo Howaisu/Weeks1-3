eec1fb1603d01c3642fe6efbe8d1702c448d85c6
XiangqiMeng <m332296992@gmail.com>
2025-01-26 16:19:53 -0500

    Oooops, just update to the wrong branch

--- Assets/Script_Assignment/NPCLogic.cs
+++ Assets/Script_Assignment/NPCLogic.cs
@@ -26,34 +26,42 @@ public class NPCLogic : MonoBehaviour
         {
             GoToBall();
         }
+        if (player.state == 2)
+        {
+            KickTheBall();
+        }
         
 
     }
 
     void GoToBall()
     {
-        if (ball == null)
+        if (ball != null)
         {
-            Debug.LogWarning("Ball is not assigned!");
-            return;
-        }
 
-        // Collect the position of the ball, and normalize the location
-        Vector3 direction = (ball.position - transform.position).normalized;
 
-        // Move towards the ball
-        transform.position += direction * Speed * Time.deltaTime;
 
-        // Wheather she get it?
-        if (Vector3.Distance(transform.position, ball.position) < 0.1f)
-        {
-            player.state = 2;
-            transform.position = ball.position; // Ensure the position again
-            Debug.Log("NPC has reached the ball!");
+            // Collect the position of the ball, and normalize the location
+            Vector3 direction = (ball.position - transform.position).normalized;
+
+            // Move towards the ball
+            transform.position += direction * Speed * Time.deltaTime;
+
+            // Wheather she get it?
+            if (Vector3.Distance(transform.position, ball.position) < 0.1f)
+            {
+                player.state = 2;
+                transform.position = ball.position; // Ensure the position again
+                Debug.Log("NPC has reached the ball!");
+            }
+            else
+            {
+                player.state = 1;
+            }
         }
-        else
-        {
-            player.state = 1;
-        }
+    }
+    void KickTheBall()
+    { 
+    
     }
 }
