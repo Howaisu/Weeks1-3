1af8e81b8598deca6bb81d9fb7da80480bc6ab83
XiangqiMeng <m332296992@gmail.com>
2025-01-26 19:53:24 -0500

    walking almost done

--- Assets/Script_Assignment/clockSwing.cs
+++ Assets/Script_Assignment/clockSwing.cs
@@ -7,8 +7,10 @@ public class clockSwing : MonoBehaviour
     //Reference
     public playerController player; // Get script of playerController
     //Basic Data
-    public float timeSpeed = 120f; //Swing Speed
+    public float swingSpeed = 120f; //Swing Speed
     public float maxAngle = 85f; // Half of the max angle
+
+    private float timeCounter = 0f; // 时间计数器
     // The Switch
     private bool isSwingingForward = true; // checking: swing to right or swing to left
 
@@ -19,28 +21,14 @@ public class clockSwing : MonoBehaviour
         Vector3 rotating = transform.eulerAngles;
         if (player.isMoving)
         {
-            // Z axis: When swing to right
-            if (isSwingingForward)
-            {
-                rotating.z += timeSpeed * Time.deltaTime; // go right
-                if (rotating.z >= maxAngle)
-                {
-                    rotating.z = maxAngle; // the maximum angle
-                    isSwingingForward = false; // switch
-                }
-            }
-            else
-            {
-                rotating.z -= timeSpeed * Time.deltaTime; // go left
-                if (rotating.z <= 360 - maxAngle && rotating.z > 180) //deal with the angle
-                {
-                    rotating.z = 360 - maxAngle; // smallest angle
-                    isSwingingForward = true; // switch
-                }
-            }
+            // The swinging speed
+            timeCounter += swingSpeed * Time.deltaTime;
 
-            // Update
-            transform.eulerAngles = rotating;
+            // Using Mathf.Sin to Calculate the current Angle
+            float angle = Mathf.Sin(timeCounter) * maxAngle;
+
+            // apply to the rotation of z-axis
+            transform.localRotation = Quaternion.Euler(0, 0, angle);
         }
     }
 }
